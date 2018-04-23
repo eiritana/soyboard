969cfca2c2d93e036adbf91f7e198212f9ed5599
Lily Mayfield <lily.m.mayfield@gmail.com>
2018-04-23 02:29:07 -0500

    Fix bug with copying db due to ordering

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
+docker cp "$(docker ps -a -q  --filter ancestor=soyboard)":/app/soyboard/test.db .
 docker stop "$(docker ps -a -q  --filter ancestor=soyboard)"
 docker-compose build
-docker cp "$(docker ps -a -q  --filter ancestor=soyboard)":/app/soyboard/test.db .
 docker-compose run -d --rm -p 0.0.0.0:80:80 soyboard
 docker cp test.db "$(docker ps -a -q  --filter ancestor=soyboard)":/app/soyboard/
