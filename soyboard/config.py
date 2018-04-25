1d1e3c5db92e20c20d6a3ab8e8a10f6cd20c47b1
Lily Mayfield <lily.m.mayfield@gmail.com>
2018-04-25 18:45:58 -0500

    Configurable through environ

--- soyboard/config.py
+++ soyboard/config.py
@@ -1,9 +1,12 @@
 import os
 
-SECRET_KEY = "PLEASE CHANGE ME"
-SQLALCHEMY_DATABASE_URI = os.environ.get('DB_STRING', 'sqlite:///test.db')
-SITE_TAGLINE = 'some tagline'
-SITE_TITLE = 'super title'
-SITE_FOOTER = '<a href="https://github.com/lily-mayfield/soyboard">Powered by Soyboard</a>'
-POSTS_PER_PAGE = 10
-INDEX_REPLIES_PER_POST = 3
+SECRET_KEY = os.environ.get('SOY_SECRET_KEY', 'PLEASE CHANGE ME')
+SQLALCHEMY_DATABASE_URI = os.environ.get('SOY_DB_STRING', 'sqlite:///test.db')
+SITE_TAGLINE = os.environ.get('SOY_SITE_TAGLINE', 'some tagline')
+SITE_TITLE = os.environ.get('SOY_SITE_TAGLINE', 'super title')
+SITE_FOOTER = os.environ.get(
+    'SOY_SITE_FOOTER',
+    '<a href="https://github.com/lily-mayfield/soyboard">Powered by Soyboard</a>',
+)
+POSTS_PER_PAGE = int(os.environ.get('SOY_POSTS_PER_PAGE', 10))
+INDEX_REPLIES_PER_POST = int(os.environ.get('SOY_INDEX_REPLIES_PER_POST', 3))
