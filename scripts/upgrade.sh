be64bc650bd37f5d813964c9bc5a02e474b5ab44
Lily Mayfield <lily.m.mayfield@gmail.com>
2018-04-24 23:20:35 -0500

    Fix for variable expansion bash

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # FIXME: this is a temporary script until I ustart storing
 # test.db outside the docker container or until I setup postgres service...
-docker cp "$(docker ps -a -q  --filter ancestor=soyboard)":/app/soyboard/test.db .
+docker cp $(docker ps -a -q  --filter ancestor=soyboard):/app/soyboard/test.db .
 docker stop "$(docker ps -a -q  --filter ancestor=soyboard)"
 docker-compose build
 docker-compose run -d --rm -p 0.0.0.0:80:80 soyboard
-docker cp test.db "$(docker ps -a -q  --filter ancestor=soyboard)":/app/soyboard/
+docker cp test.db $(docker ps -a -q  --filter ancestor=soyboard):/app/soyboard/
