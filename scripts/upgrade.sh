2a0a9df6a361f84674b4fe6678e02e7d63dcec9f
Lily Mayfield <lily.m.mayfield@gmail.com>
2018-04-25 18:55:13 -0500

    Whoops! Use regular CP not Docker CP!

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -3,7 +3,7 @@
 # setup right yet for this repo.
 
 echo "Backup database to /tmp..."
-docker cp soyboard/test.db /tmp
+cp soyboard/test.db /tmp
 
 echo "Build new Docker image!"
 docker-compose build
@@ -15,4 +15,4 @@ echo "Run new image and serve!"
 docker-compose run -d --rm -p 0.0.0.0:80:80 soyboard
 
 echo "Copy database from backup to Docker location!"
-docker cp /tmp/test.db soyboard/test.db
+cp /tmp/test.db soyboard/test.db
