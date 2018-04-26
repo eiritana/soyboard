23dfb341a32989aba5317de60e24fdaa5822ed82
Lily Mayfield <lily.m.mayfield@gmail.com>
2018-04-25 19:14:01 -0500

    Fix typo (needed --, had -); set -e

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,6 +2,8 @@
 # FIXME: this will be deleted soon because Docker isn't
 # setup right yet for this repo.
 
+set -e
+
 echo "Backup database to /tmp..."
 cp soyboard/test.db /tmp
 
@@ -12,7 +14,7 @@ echo "Stop any currently running soyboard containers..."
 docker stop "$(docker ps -a -q  --filter ancestor=soyboard)"
 
 echo "Run new image and serve!"
-docker-compose run -env-file ./.env-file -d --rm -p 0.0.0.0:80:80 soyboard
+docker-compose run --env-file ./.env-file -d --rm -p 0.0.0.0:80:80 soyboard
 
 echo "Copy database from backup to Docker location!"
 cp /tmp/test.db soyboard/test.db
